d8e4602334bc1e95d6ea6d441c479711a25a0fc2
Heather Pekrul <heather.pekrul@gmail.com>
2021-09-07 19:52:49 -0500

    Add manifest

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 
   <meta name="viewport" content="width=device-width,initial-scale=1">
   <meta name="theme-color" content="#17f9ff">
+  <link rel="manifest" href="manifest.json">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
